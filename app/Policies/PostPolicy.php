3295d1fadd0e6e288c4a4c0cac8b9dcbf2089598
Song Jae Hyeon <0nocsace@gmail.com>
2021-07-07 20:49:47 +0900

    Laravel 07/07 Policies 추가

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PostPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Post  $post
+     * @return mixed
+     */
+    public function view(User $user, Post $post)
+    {
+        //두개 인자를 가짐.
+        // 이 사용자가 뷰를 볼 권한이 있나?
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        // 이 사용자가 게시글 만들권한?
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Post  $post
+     * @return mixed
+     */
+    public function update(User $user, Post $post)
+    {
+        //ㅇㅇ위에
+
+        return $user->id === $post->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Post  $post
+     * @return mixed
+     */
+    public function delete(User $user, Post $post)
+    {
+        //ㅇ위에
+        return $user->id === $post->user_id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Post  $post
+     * @return mixed
+     */
+    public function restore(User $user, Post $post)
+    {
+        //delete 하지말고 ,  delete 한것은delete 했다고 표시만 하는 soft delete
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Post  $post
+     * @return mixed
+     */
+    public function forceDelete(User $user, Post $post)
+    {
+        //softdelete 아니고 영구삭제임
+    }
+}
