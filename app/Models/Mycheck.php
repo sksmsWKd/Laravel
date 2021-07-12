34fe65349812d2ab560df7528b4ea70e83e973fb
Song Jae Hyeon <0nocsace@gmail.com>
2021-07-12 20:36:18 +0900

    Laravel 07/12 Add Checklist 기능모델 추가

---
+++ app/Models/Mycheck.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Mycheck extends Model
+{
+
+    protected $table = 'mychecks';
+    use HasFactory;
+}
