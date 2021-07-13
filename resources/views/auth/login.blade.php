aef6d6a461fe571d5933dc8c3655a7cb24f1d9c2
Song Jae Hyeon <0nocsace@gmail.com>
2021-07-13 18:05:16 +0900

    07/13  Laravel Login Form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
         .font-sans.text-gray-900.antialiased {
             background-color: rgb(30, 32, 34) !important;
@@ -75,8 +86,28 @@
                         {{ __('Log in') }}
                     </x-button>
                 </div>
+                <div>
+
+                    <br>
+                    <br>
+                    <button type="button" class="btn btn-dark" id="img_btn"
+                        onclick="location.href=' {{ route('github.login') }}'"> <img
+                            src="/storage/images/{{ $post->image ?? 'github.png' }}"></button>
+                    &nbsp;&nbsp;
+                    <button type="button" class="btn btn-dark" id="img_btn"> <img
+                            src="/storage/images/{{ $post->image ?? 'google.png' }}"></button>
+                    &nbsp;&nbsp;
+                    <button type="button" class="btn btn-dark" id="img_btn"> <img
+                            src="/storage/images/{{ $post->image ?? 'kakao.jpg' }}"></button>
+                </div>
+
+
             </form>
         </x-auth-card>
+
+
+
+
     </x-guest-layout>
 
 </body>
