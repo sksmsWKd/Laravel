e342482aeea5113be583d0f8813a4a87836aeaea
Song Jae Hyeon <0nocsace@gmail.com>
2021-07-25 16:00:43 +0900

    07/25 Laravel

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -17,6 +17,11 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+
+        ],
+        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+            // ... other providers
+            'SocialiteProviders\\Kakao\\KakaoExtendSocialite@handle',
         ],
     ];
 
