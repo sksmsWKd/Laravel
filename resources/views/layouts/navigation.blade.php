37eea2cb1c086d953e3f7055cfaca4a60df1233c
Song Jae Hyeon <0nocsace@gmail.com>
2021-07-26 19:04:41 +0900

    07/26 Laravel Navigation color function

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -39,6 +39,14 @@
             background-color: rgb(24, 26, 27);
         }
 
+        #MyClockDisplay {
+            color: lavender;
+            margin-top: 1.39em;
+            font-family: Orbitron;
+            letter-spacing: 5px;
+
+        }
+
     </style>
 
 
@@ -61,7 +69,10 @@
                         </div>
 
                         <!-- Navigation Links -->
+
+
                         <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+
                             <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                                 <div class="text-gray-300">
                                     {{ __('DASHBOARD') }}
@@ -91,6 +102,39 @@
                                     {{ __('SEARCH') }}
                                 </div>
                             </x-nav-link>
+
+                            <div id="MyClockDisplay" class="clock" onload="showTime()"></div>
+                            <script>
+                                function showTime() {
+                                    var date = new Date();
+                                    var h = date.getHours(); // 0 - 23
+                                    var m = date.getMinutes(); // 0 - 59
+                                    var s = date.getSeconds(); // 0 - 59
+                                    var session = "AM";
+
+                                    if (h == 0) {
+                                        h = 12;
+                                    }
+
+                                    if (h > 12) {
+                                        h = h - 12;
+                                        session = "PM";
+                                    }
+
+                                    h = (h < 10) ? "0" + h : h;
+                                    m = (m < 10) ? "0" + m : m;
+                                    s = (s < 10) ? "0" + s : s;
+
+                                    var time = h + ":" + m + ":" + s + " " + session;
+                                    document.getElementById("MyClockDisplay").innerText = time;
+                                    document.getElementById("MyClockDisplay").textContent = time;
+
+                                    setTimeout(showTime, 1000);
+
+                                }
+                                showTime();
+                            </script>
+
 
                             {{-- @auth
                         <x-nav-link :href="route('posts.create')" :active="request()->routeIs('posts.create')">
