9100626eef7c273f68ff3681f5e62a62d4006981
Song Jae Hyeon <0nocsace@gmail.com>
2021-07-31 21:16:20 +0900

    Laravel add comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+
+    protected $table = 'comments';
     use HasFactory;
 
-    protected $fillable = ['content', 'userName', 'userId'];
+    protected $fillable = ['content'];
 }
