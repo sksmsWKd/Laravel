1b15eec3b60f28221551c0fc0c3122eb439f9d8b
Song Jae Hyeon <0nocsace@gmail.com>
2021-07-31 21:17:03 +0900

    Laravel show comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,30 +6,28 @@ use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class CommentController extends Controller
 {
-    public function commentSave(Request $request)
+    public function commentSave(Request $request, $id)
     {
 
 
 
-
-
-
-
-
+        $post = Post::findOrFail($id);
         $request->validate([
             'content' => 'required|max:100',
-            'userName' => 'required',
-            'userId' => 'required'
+
         ]);
 
+
+
         $making = new Comment();
-        $making->content =  $request->content;
-        $making->userName = Auth::user()->name;
-        $making->userId = Auth::user()->id;
+        $making->content  = $request->input('content');
+        $making->post_id = $post->id;
+
 
         $making->save();
         return redirect();
