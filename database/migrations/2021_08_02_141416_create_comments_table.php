fab8da2e1b7d695237fd86395f9ee8119c30ec77
Song Jae Hyeon <0nocsace@gmail.com>
2021-08-04 18:12:22 +0900

    Laravel comment table

---
+++ database/migrations/2021_08_02_141416_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('cID');
+            $table->timestamps();
+            $table->string('content', 100);
+
+
+
+
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
+//아직 migrate 안함.
