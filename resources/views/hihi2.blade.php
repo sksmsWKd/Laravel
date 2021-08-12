90fe5d18b9142b29165a581ac4ac22dfbbf9fb83
Song Jae Hyeon <0nocsace@gmail.com>
2021-08-12 20:52:46 +0900

    Blade.php yield test

---
+++ resources/views/hihi2.blade.php
@@ -0,0 +1,19 @@
+@extends('hihi')
+
+@section('header')
+    header입니다
+@stop
+
+
+@section('body')
+    <p>body입니다</p>
+@stop
+
+@section('footer')
+    <p>footer입니다</p>
+@stop
+
+
+
+
+@extends('hihi')
