37b9db4959f4330855e3c8132a81c49f92b64ac1
Song Jae Hyeon <0nocsace@gmail.com>
2021-08-12 20:52:34 +0900

    Blade.php yield test

---
+++ resources/views/hihi.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>f</title>
+</head>
+
+<body>
+
+    @yield('header')
+    @yield('body')
+    @yield('footer')
+
+
+    --1회
+    <br>
+</body>
+
+</html>
