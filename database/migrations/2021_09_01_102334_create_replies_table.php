2ee0fc8b41ae7e9f5b28e057d7458cf0a8ed7571
Song Jae Hyeon <0nocsace@gmail.com>
2021-09-01 22:42:03 +0900

    create reply

---
+++ database/migrations/2021_09_01_102334_create_replies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('replyContent', 100);
+
+            $table->string('user_name');
+
+            $table->integer('comment_id')->foriegnId('comment_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
